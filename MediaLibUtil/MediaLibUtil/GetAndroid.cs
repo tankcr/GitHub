fbf68de99c09cba23d01ef5a2705ee223ed1ca89
tankcr <kristopher.roy@gmail.com>
2014-07-28 14:15:37 -0700

    added files

---
+++ MediaLibUtil/MediaLibUtil/GetAndroid.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using RegawMOD.Android;
+
+
+namespace MediaLibUtil
+{
+    public class Devices
+    {
+
+        AndroidController android;
+        Device device;
+
+        public event EventHandler TableDataChanged;
+
+        private List<Device> tableData;
+        public List<Device> TableData
+        {
+            get { return tableData; }
+            set
+            {
+                tableData = value;
+                if (TableDataChanged != null)
+                {
+                    TableDataChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+        public Devices()
+        {
+
+        }
+    }
+    public class GetDevices
+    {
+        public static AndroidController GetDeviceData()
+        {
+            AndroidController android;
+            Device device;
+            android = AndroidController.Instance;
+            if (android.HasConnectedDevices)
+            {
+                int num = android.ConnectedDevices.Count;
+                for (int i = 0; i < android.ConnectedDevices.Count; i++)
+                {
+                    string serial = android.ConnectedDevices[i];
+                    device = android.GetConnectedDevice(serial);
+                    string devserial = device.SerialNumber;
+                    Phone phone = device.Phone;
+                    DeviceState state = device.State;
+                    string test = "\"";
+                    string prodcmd = @"shell cat /system/build.prop | grep ""product""";
+                    Debug.WriteLine("Single quote: " + @"""");
+                    Debug.WriteLine(@"Single quote: "" .");
+                    AdbCommand adbCmdCard = Adb.FormAdbCommand(device, "shell df /storage/*Card*");
+                    AdbCommand adbCmdcard = Adb.FormAdbCommand(device, "shell df /storage/*card*");
+                    AdbCommand adbCmddisc = Adb.FormAdbCommand(device, prodcmd);
+                    string exp = @"\s+";
+                    //AdbCommand cmd;
+                    //cmd = Adb.FormAdbCommand(device, "shell df /storage/*card*", null);
+                    string Card = Adb.ExecuteAdbCommand(adbCmdCard);
+                    string card = Adb.ExecuteAdbCommand(adbCmdcard);
+                    string descript = Adb.ExecuteAdbCommand(adbCmddisc);
+                    string[] group1 = Regex.Split(Card, exp);
+                    string[] group2 = Regex.Split(card, exp);
+
+                }
+            }
+            return android;
+        }
+    }
+}
---
+++ shpInfo/SharePointCommands/SharePointCommands/GetFarm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Threading;
+
+namespace SharePointCommands
+{
+    public class ShPFarm
+    {
+        public event EventHandler TableDataChanged;
+
+        private List<string> tableData;
+        public List<string> TableData
+        {
+            get { return tableData; }
+            set
+            {
+                tableData = value;
+                if (TableDataChanged != null)
+                {
+                    TableDataChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public ShPFarm()
+        {
+
+        }
+    }
+    public class GetFarm
+    {
+        public static ShPFarm GetFarmData(string url)
+        {
+            ShPFarm table = new ShPFarm();
+            table.TableData = new List<string>();
+            PowerShell command = PowerShell.Create();
+            command.AddScript(@"$SPOCred = (GCI C:\*\SPOCreds.PS1).fullname
+            invoke-expression -Command $SPOCred
+            $servers = Get-Content '\\SRVCORDC01\Scripts\SharePoint\Management\ProductionServers.txt'
+            foreach($server in $servers)
+            {
+                Enable-WSManCredSSP Client –DelegateComputer $server -Force
+                invoke-command -ComputerName $server{Enable-WSManCredSSP Server -Force}
+            }");
+            PSDataCollection<PSObject> outputCollection = new PSDataCollection<PSObject>();
+            outputCollection.DataAdded += outputCollection_DataAdded;
+            command.Streams.Error.DataAdded += Error_DataAdded;
+            IAsyncResult result = command.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+            while (result.IsCompleted == false)
+            {
+                Console.WriteLine("Waiting for pipeline to finish...");
+                Thread.Sleep(1000);
+
+                // might want to place a timeout here...
+            }
+            Console.WriteLine("Execution has stopped. The pipeline state: " + command.InvocationStateInfo.State);
+
+            foreach (PSObject outputItem in outputCollection)
+            {
+                //TODO: handle/process the output items if required
+                Console.WriteLine(outputItem.BaseObject.ToString());
+                table.TableData.Add(outputItem.BaseObject.ToString());
+            }
+            
+            return table;
+        }
+
+        private static void Error_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void outputCollection_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
